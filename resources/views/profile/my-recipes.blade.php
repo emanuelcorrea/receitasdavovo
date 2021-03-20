fb48b2a03d973ce2328b7f61dab707f0fb7b1dc6
Emanuel Corrêa <emanuel.silvati@gmail.com>
2021-03-20 19:04:42 -0300

    menu javascript & my-recipes

--- resources/views/profile/my-recipes.blade.php
+++ resources/views/profile/my-recipes.blade.php
@@ -1,5 +1,39 @@
 @extends('profile.main')
 
 @section('profile-content')
-    teste
+    <div class="profile-content__title">
+        <h2>Minhas receitas</h2>
+    </div>
+    <div class="my-recipes">
+        <div class="my-recipes__search">
+            <form action="#">
+                @csrf
+
+                <input aria-label="Pesquise sua receita" type="text" name="recipe_name" placeholder="Busque sua receita...">
+            </form>
+        </div>
+
+        <div class="my-recipes__list">
+            <ul>
+                <li class="my-recipes-item">
+                    <div class="my-recipes-item__image">
+                        <img src="http://127.0.0.1:8000/img/receitas/quentao-vinho.jpg" alt="Recipe Image">
+                    </div>
+                    <div class="my-recipes-item__options">
+                        <div class="my-recipes-item__name">
+                            <a href="#">Quentão de vinho</a>
+                        </div>
+                        <div>
+                            <div class="my-recipes-item__edit">
+                                <i></i>
+                            </div>
+                            <div class="my-recipes-item__remove">
+                                <i></i>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
 @endsection
